4e73fc4423c8c9bb0defc18ed15a9ef5e8eff607
Bengi Su Gelin <113550043+bengisugelin@users.noreply.github.com>
2023-12-01 17:17:11 -0800

    Add files via upload

---
+++ routes/hotels.js
@@ -0,0 +1,32 @@
+import express from "express";
+import Hotel from "../models/Hotel.js"
+import { createError } from "../utils/error.js";
+import { countByCity, countByType, createHotel, deleteHotel,getHotels, getHotel, updateHotel, getHotelsByCityAndPrice } from "../controllers/hotelController.js";
+
+
+const router = express.Router();
+
+
+// CREATE
+router.post("/", createHotel);
+//UPDATE
+
+router.put("/:id",  updateHotel);
+
+//DELETE
+
+router.delete("/:id",  deleteHotel);
+
+//GET
+router.get("/find/:id", getHotel);
+
+//GET ALL
+
+router.get("/", getHotels);
+
+router.get("/countByCity", countByCity);
+
+router.get("/countbyType", countByType);
+
+router.get("/getbycitynameandprice", getHotelsByCityAndPrice)
+export default router
